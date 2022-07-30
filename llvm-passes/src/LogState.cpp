a1b07abc16d74237b5a51dd222760fded017fa24
afnan-s <afnan.s@gmail.com>
2022-07-30 19:50:46 +0100

    minor fixes

--- llvm-passes/src/LogState.cpp
+++ llvm-passes/src/LogState.cpp
@@ -468,8 +468,7 @@ bool LogState::runOnModule(Module &M) {
     //DEBUG
     
     // Loop through all basic blocks in function F
-    // for (auto &BB : F) {
-    for (auto BB = F.begin(), BE = F.end(); BB != BE; ++BB){
+    for (auto &BB : F) {
 
       // Loop over all instructions in the block. Inst iterator also
       // helps locating instructions for insertion.
@@ -477,10 +476,9 @@ bool LogState::runOnModule(Module &M) {
         errs() << "Inside Inst loop. Considering: " << *Inst << "\n";
 
         // Check if first instruction in a function:
-        // BasicBlock* firstBB = &*(F.begin());
+        BasicBlock* firstBB = &*(F.begin());
 
-        // if(Inst == BB.begin() && BB == firstBB){
-        if(Inst == BB.begin() && BB == F.begin()){
+        if(Inst == BB.begin() && &BB == firstBB){
           IRBuilder<> Builder(&*(Inst));
           insertFunCallCounterIncrement(BB, &Builder);
 
