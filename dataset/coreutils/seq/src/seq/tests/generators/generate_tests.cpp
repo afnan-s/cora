703b9a673696504e2d19245b7516756707d54f68
afnan-s <afnan.s@gmail.com>
2022-08-09 11:56:20 +0100

    Added seq

--- dataset/coreutils/seq/src/seq/tests/generators/generate_tests.cpp
+++ dataset/coreutils/seq/src/seq/tests/generators/generate_tests.cpp
@@ -13,13 +13,13 @@ using namespace std;
 
 int set_size = 2000;
 
-int rand_int(){
+int rand_int(int min, int max){
 	int randomVar;
 
 	//Random integer generator for replacement:
 	random_device dev;
   	mt19937 RNG_int(dev());
-  	uniform_int_distribution<> distInt(0,INT_MAX); // distribution in range [INT_MIN,INT_MAX]
+  	uniform_int_distribution<> distInt(min,max); // distribution in range [INT_MIN,INT_MAX]
   	//Assign to global variable:
   	randomVar = distInt(RNG_int);
 
@@ -30,15 +30,35 @@ int rand_int(){
 
 int main(){
 	string path = "../uniform/";
-	set<int> inputs;
-
+	string input;
+	set<string> inputs;
+	int last;
+	int first;
+	int step;
 	while(inputs.size() < set_size){
-		int randInt = rand_int();
-		inputs.insert(randInt);
+		// First randomly decide how many inputs:
+		int num_input = rand_int(1, 3);
+
+		// Generate _last_ input:
+		last = rand_int(INT_MIN, INT_MAX);
+
+		if(num_input > 1) {
+			first = rand_int(INT_MIN, INT_MAX);
+			input+=to_string(first)+",";
+		}
+
+		if(num_input > 2){
+			step = rand_int(INT_MIN, INT_MAX);
+			input+=to_string(step)+",";
+		}
+
+		input += to_string(last);
+		inputs.insert(input);
 	}
 
+	
 	int counter = 1;
-	for(int input : inputs){
+	for(string input : inputs){
 		ofstream input_file(path+std::to_string(counter)+".in");
 		input_file << input;
 		input_file.close();
